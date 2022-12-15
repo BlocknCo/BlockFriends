ecc6ff67eab00a0fd4881433ca54305b4331e4ef
WltrH <wltrhrng@gmail.com>
2022-12-15 19:01:49 +0100

    suite smart contract

--- hardhat/contracts/Petsfriends.sol
+++ hardhat/contracts/Petsfriends.sol
@@ -37,7 +37,27 @@ contract Petsfriends is ERC721Enumerable, ownable {
         _;
     }
 
-    //rajout d'event de Mint, à voir avec le contrat 721
+    constructor (string memory baseURI, address whiteListContract) ERC721("Pets Friends", "PF") {
+        _baseTokenURI = baseURI;
+        whitelist = IWhitelist(whiteListContract);
+    } 
+
+
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp < presaleEnded, "Presale is not running");
+        require(whitelist.whitelistedAddresses(msg.sender), "You are not whitelisted");
+        // voir si on garde ce require car le but étant de mettre un NFT par animal
+        // voir aussi pour vérifier que l'animal ne peut être mis qu'une seule fois
+        require(tokenIds < maxTokenIds, "Exceeded maximum Pets Friends supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        _safeMint(msg.sender, tokenIds);
+    }   
+//rajout d'event de Mint, à voir avec le contrat 721
 
 /**Dans le NFT
 
